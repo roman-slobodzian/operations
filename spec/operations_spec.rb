5d06fd7fcfd24137afe7917839bda757f71ad978
Roman Slobodzian <romsssa@gmail.com>
2021-05-30 10:39:53 +0300

    Initialize a new gem

---
+++ spec/operations_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Operations do
+  it "has a version number" do
+    expect(Operations::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
