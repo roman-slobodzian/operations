e10da4d2073b32388d9e58c3ba2cc1531ab41b31
Roman Slobodzian <romsssa@gmail.com>
2021-07-17 14:04:40 +0300

    Fix normalizer_class method

--- lib/operations/operation/normalizer.rb
+++ lib/operations/operation/normalizer.rb
@@ -9,15 +9,17 @@ module Operations
         def normalize(query: nil)
           normalizer_class.normalize(self[:resource], query: query)
         end
+
+        private
+
+        def normalizer_class
+          @normalizer_class ||= self.class.normalizer_class || "#{resource_name}Normalizer".constantize
+        end
       end
 
       class_methods do
         def normalizer_class!(klass)
           self.normalizer_class = klass
-        end
-
-        def normalizer_class
-          @normalizer_class ||= self.class.normalizer_class || "#{resource_name}Normalizer".constantize
         end
       end
     end
